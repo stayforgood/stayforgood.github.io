16a05dfdd43bcd7b566d6546c7abce298a28398c
Jack Morris <jxmorris12@gmail.com>
2017-04-13 19:12:14 -0400

    font change and disable 3d for now

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var scrollTo = function(elementID) {
+  // (document.body).animate({
+    // 'scrollTop':   $('#' + elementID).offset().top
+  // }, 2000);
+
+  // location.hash = '#' + elementID;
+
+  // console.log('#' + elementID);
+  // $(document.body).scrollTop($('#' + elementID));
+
+  $('html, body').animate({
+     'scrollTop':   $('#' + elementID).offset().top
+   }, 2000);
+}
